6bd60c4c33e254ea29ea26cf6bb753804199c758
mforv <master-forve@ya.ru>
2018-05-30 23:30:02 +0500

    Update 1.01: UI scaling, timer, exit codes, mouse tracking

--- luk_op_proto.py
+++ luk_op_proto.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import time
+import math
 import datetime
 
 from PyQt5 import QtGui, QtCore, QtWidgets
@@ -14,12 +15,21 @@ elems = {}
 # Override-класс интерфейса для реализации обработки кастомных сигналов
 class LukWidget(QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
-    def __init__(self, contoller):
+    resized = QtCore.pyqtSignal()
+
+    def __init__(self, controller):
         super(LukWidget, self).__init__()
         self.setupUi(self)
-        self.sc = contoller
+        self.resized.connect(self.scale_elems)
+        self.sc = controller
         self.sc.new_log_entry.connect(self.new_log_entry)  # соединяем один слот с сигналом
         self.sc.new_temp_value.connect(self.new_temp_value)  # другой слот с сигналом
+        self.sc.scenario_ended.connect(self.stop_controller)
+        self.w = self.window().width()
+        self.h = self.window().height()
+        children = self.findChildren(QtWidgets.QWidget)
+        for child in children:
+            child.setMouseTracking(True)
     
     def new_log_entry(self, msg):
         self.log.append(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + ' ' + str(msg))
@@ -41,6 +51,38 @@ class LukWidget(QtWidgets.QMainWindow, gui.Ui_MainWindow):
         self.ctgt.setPalette(tmp_plt)
         self.label_8.setPalette(tmp_plt)
         self.ctgt.display(value)
+    
+    def stop_controller(self, code):
+        self.log.append("=== Сценарий завершен ===")
+        self.log.append("Код завершения: " + str(code))
+        self.sc.exit()
+    
+    def resizeEvent(self, event):
+        self.resized.emit()
+        return super(LukWidget, self).resizeEvent(event)
+    
+    def scale_elems(self):
+        children = self.centralwidget.findChildren(QtWidgets.QWidget)
+        w1 = self.window().width()
+        h1 = self.window().height()
+        h_scale_rate = h1 / self.h
+        for widget in children:
+            if widget.height() > 0:
+                aspect =  widget.width() / widget.height()
+            else:
+                aspect = 1
+            new_height = widget.height() * h_scale_rate
+            new_width = new_height * aspect
+            widget.setGeometry(round(widget.geometry().x() * h_scale_rate), round(widget.geometry().y() * h_scale_rate), new_width, new_height)
+        self.w = w1
+        self.h = h1
+
+    def mouseMoveEvent(self, event):
+        super().mouseMoveEvent(event)
+        pos = event.pos()
+        print("Mouse at " + str(pos.x()) + ", " + str(pos.y()), file=sys.stderr)
+        #self.log(pos)
+
 
 # Класс, минимально описыващий элементы установки для реализации логики
 class Element:
@@ -55,7 +97,7 @@ class Element:
         if self.widget is not None:
             self.update_colors(self.state)
         else:
-            print('Warning: Nothing to change')
+            print('Warning: Nothing to change', file=sys.stderr)
     
     def update_colors(self, color_state = 0):
         '''Перерисовываем спрайт с наложением соответствующего состоянию цвета'''
@@ -74,7 +116,7 @@ class Element:
             p.end()
             self.widget.setPixmap(pxm)
         else:
-            print('Warning: Nothing to update')
+            print('Warning: Nothing to update', file=sys.stderr)
     
     def set_state(self, new_state, condition=None, *args, **kwargs):
         '''Установить состояние вручную'''
@@ -112,28 +154,31 @@ class ScenarioController(QtCore.QThread):
 
     new_log_entry = QtCore.pyqtSignal(object)  # сигнал для обновления лога
     new_temp_value = QtCore.pyqtSignal(float, float)  # сигнал для обновления цифрового табло
+    scenario_ended = QtCore.pyqtSignal(int)  # сигнал для завершения сценария
 
-    def __init__(self, temp=60, critical_temp=200):
+    def __init__(self, temp=60, critical_temp=200, timer=60.0):
         QtCore.QThread.__init__(self)
         self.active = False
         self.temp = temp
         self.crit_t = critical_temp
         self.action_handler = []
         self.start_time = datetime.datetime.now().timestamp()
-        self.current_time = datetime.datetime.now().timestamp()
+        self.current_time = self.start_time
+        self.timer = timer
     
     def __del__(self):
         self.wait()
 
     def execute_scenario(self, scenario): # последовательно выполнить все действия, если выполнены их условия, в сценарии scenario
         for action in scenario.keys():
-            self.action_handler.append([scenario[action], 0])
+            self.action_handler.append([scenario[action], 0, self.current_time])
         self.active = True
         while self.active:
             for action in self.action_handler:
                 if action[0][1] is not None and callable(action[0][1]):                             # если действие повторяется, пока выполняется условие
-                    if action[0][1]() and (self.current_time - self.start_time) >= action[0][2]:    # если условие выполнено и при этом прошло достаточно времени для повтора
+                    if action[0][1]() and (self.current_time - action[2]) >= action[0][2]:          # если условие выполнено и при этом прошло достаточно времени для повтора
                         action[0][0]()                                                              # выполняем действие
+                        action[2] = self.current_time                                               # указываем время последнего выполнения
                     elif not action[0][1]():                                                        # если условие не выполнено 
                         action[1] += 1                                                              # помечаем, что действие больше не нужно выполнять
                     else:
@@ -145,12 +190,17 @@ class ScenarioController(QtCore.QThread):
                     else:
                         continue        # если нет, идем дальше
             self.current_time = datetime.datetime.now().timestamp()
-            self.sleep(1)
+            self.sleep(0.01)
     
     def log(self, msg):
         '''Логирование в консоль и в интерфейс'''
         print(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + ' ' + str(msg))
         self.new_log_entry.emit(msg)
+    
+    def reset_timer(self, new_timer=60.0):
+        self.timer = new_timer
+        self.start_time = datetime.datetime.now().timestamp()
+        self.log("Был сброшен таймер, новое время для выполнения задания: " + str(self.timer) + " сек.")
     
     def set_crit_t(self, new_crit_t):
         self.crit_t = new_crit_t
@@ -165,7 +215,7 @@ class ScenarioController(QtCore.QThread):
         self.display_temp()
         self.log("Значение температуры установлено на %s" % new_temp)
     
-    def check(self, cond_desc='[условие перехода без описания]', condition=None, *args, **kwargs,):
+    def check(self, cond_desc='[условие перехода без описания]', condition=None, *args, **kwargs):
         '''Проверить выполнения условия перехода и вернуть результат проверки и описание условия для журнала'''
         if condition is not None and callable(condition):
             cond = condition(*args, **kwargs)
@@ -190,11 +240,14 @@ class ScenarioController(QtCore.QThread):
 
     def make_call(self):
         '''Сымитировать звонок диспетчеру'''
-        if elems['сс'] is not None:
-            elems['сс'].set_state(0)
         self.active = False
         self.log("Сделан звонок диспетчеру")
-        self.log("=== Сценарий завершен ===")
+        self.scenario_ended.emit(0)
+    
+    def fail(self):
+        self.active = False
+        self.log("Задание не выполнено в срок")
+        self.scenario_ended.emit(1)
 
     def run(self):
         '''Запуск сценария'''
@@ -232,22 +285,23 @@ if __name__ == '__main__':
         "сброс_состояния_втг": (lambda: elems['втг'].set_state(1), None, 0),
         "сброс_состояния_запг": (lambda: elems['запг'].set_state(1), None, 0),
         "сброс_состояния_загб": (lambda: elems['загб'].set_state(1), None, 0),
+        "сброс_таймера": (lambda: sc.reset_timer(), None, 0),
         "сброс_завершение": (lambda: sc.log(' === Сброс состояний завершен ==='), None, 0),
-        #"старт_сценария": (lambda: sc.set_temp(20.7), None, 0),
-        "сс_при_критическом_т": (lambda: elems['сс'].set_state(1, lambda: sc.check('Т > Ткрит', lambda: sc.temp > sc.crit_t)), lambda: elems['сс'].state != 1, 5),
+        "сс_при_критическом_т": (lambda: elems['сс'].set_state(1, lambda: sc.check('Т > Ткрит', lambda: sc.temp > sc.crit_t)), lambda: elems['сс'].state != 1, 1),
         "повышение_температуры": (lambda: sc.raise_temp(60.45), lambda: sc.temp < sc.crit_t and 
-                                                                                        elems['зугт'].state == 0 and 
-                                                                                        elems['зуку'].state == 0 and
-                                                                                        elems['втг'].state == 1 and
-                                                                                        elems['запг'].state == 1 and
-                                                                                        elems['загб'].state == 1, 5),
+                                                                        elems['зугт'].state == 0 and 
+                                                                        elems['зуку'].state == 0 and
+                                                                        elems['втг'].state == 1 and
+                                                                        elems['запг'].state == 1 and
+                                                                        elems['загб'].state == 1, 2),
         "снижение_температуры_при_правильных_действиях": (lambda: sc.lower_temp(10.25), lambda: sc.temp > 60 and 
-                                                                                        elems['зугт'].state == 1 and 
-                                                                                        elems['зуку'].state == 1 and
-                                                                                        elems['втг'].state == 0 and
-                                                                                        elems['запг'].state == 0 and
-                                                                                        elems['загб'].state == 0, 5),
-        "возврат_сс_в_норму": (lambda: elems['сс'].set_state(0, lambda: sc.check('Т < Ткрит', lambda: sc.temp < sc.crit_t)), lambda: elems['сс'].state == 1, 5),
+                                                                                                elems['зугт'].state == 1 and 
+                                                                                                elems['зуку'].state == 1 and
+                                                                                                elems['втг'].state == 0 and
+                                                                                                elems['запг'].state == 0 and
+                                                                                                elems['загб'].state == 0, 1),
+        "возврат_сс_в_норму": (lambda: elems['сс'].set_state(0, lambda: sc.check('Т < Ткрит', lambda: sc.temp < sc.crit_t)), lambda: elems['сс'].state == 1, 1),
+        "провал_задания_по_таймеру": (lambda: sc.fail(), lambda: sc.current_time >= sc.start_time + sc.timer and sc.temp >= sc.crit_t, 0.1)
     }
 
     # Пока руками приписывание кнопкам действия
